3be6ae14e7be48ee50d66987ae029904300286a4
GokarajuKavya27 <116956986+GokarajuKavya27@users.noreply.github.com>
2023-08-21 22:10:12 +0530

    Add files via upload

---
+++ Greatest of three numbers.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a=10
+b=20
+c=2
+if(a>b)&(a>c):
+    print("a has greatest value")
+elif(b>c):
+    print("b has greatest value")
+else:
+    print("c has greatest value")
+
+
+# In[ ]:
+
+
+
+
