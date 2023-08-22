75249acac369d60081fedda190bab2dd3e61934e
GokarajuKavya27 <116956986+GokarajuKavya27@users.noreply.github.com>
2023-08-22 21:59:48 +0530

    Add files via upload

---
+++ Fibonacci Series.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#Fibonacci Series
+n1=0
+n2=1
+print(n1)
+print(n2)
+for i in range(1,11):
+    n3=n1+n2
+    print(n3)
+    n1=n2
+    n2=n3
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
