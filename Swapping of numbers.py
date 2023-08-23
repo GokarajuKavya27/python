0f803e68b1bc5e4e71906007ff83310e307dd618
GokarajuKavya27 <116956986+GokarajuKavya27@users.noreply.github.com>
2023-08-23 21:19:41 +0530

    Add files via upload

---
+++ Swapping of numbers.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+#Method-1
+a=433
+b=876
+temp=a  
+a=b
+b=temp
+print(a,b)
+   
+#Method-2    
+a=90
+b=34
+a=a+b       
+b=a-b
+a=a-b
+print(a,b)
+
+#Method-3
+a=123
+b=987
+a=a^b
+b=a^b
+a=a^b
+print(a,b)
+
+
+#Method-4
+a=23
+b=56
+(a,b)=(b,a)
+print(a,b)
+
+
+#Method-5
+a=67
+b=56
+a=a*b
+b=a//b
+a=a//b
+print(a,b)
+
+
+# In[ ]:
+
+
+
+
