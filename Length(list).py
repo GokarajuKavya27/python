8643b082257168a6a0c911ef566d3736f50d7b22
GokarajuKavya27 <116956986+GokarajuKavya27@users.noreply.github.com>
2023-08-28 22:25:03 +0530

    Add files via upload
    to find length of list

---
+++ Length(list).py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+a=[1,2,3,4]
+b=[987,"qwer",23.7,"abc","monkey",{1,2}]
+c=["k","dog","cat",10,"y","u","xxy"]
+d=[33,8909,5674,34564,"a","s","l","c","n"]
+x=len(a)
+y=len(b)
+z=len(c)
+h=len(d)
+print(x)
+print(y)
+print(z)
+print(h)
+
+
+# In[ ]:
+
+
+
+
