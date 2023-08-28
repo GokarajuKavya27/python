76110f0911195757df85cbbfe1d9cfc2a19a9aa7
GokarajuKavya27 <116956986+GokarajuKavya27@users.noreply.github.com>
2023-08-28 22:07:12 +0530

    Add files via upload

---
+++ Prime or not.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+# To take input from the user
+num = int(input("Enter a number: "))
+
+if num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+   # check for factors
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           break
+   else:
+       print(num,"is a prime number")
+       
+# if input number is less than
+# or equal to 1, it is not prime
+else:
+   print(num,"is not a prime number")
+
+
+# In[ ]:
+
+
+
+
