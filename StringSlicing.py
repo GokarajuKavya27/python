c196892e2912d98523c9b836a5583788705afbb9
GokarajuKavya27 <116956986+GokarajuKavya27@users.noreply.github.com>
2023-09-01 14:50:35 +0530

    Add files via upload

---
+++ StringSlicing.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[38]:
+
+
+a="hello world"
+b=a[0]
+c=a[-7:]
+d=a[:]
+e=a[0:8]
+f=a[-6:]
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+
+
+# In[28]:
+
+
+a="okk."
+b=a[-2]
+print(b)
+
+
+# In[25]:
+
+
+a="i love you"
+b=a[3]
+print(b)
+
+
+# In[32]:
+
+
+a="welcome to python programmimg"
+b=a[7:14]
+print(b)
+
+
+# In[39]:
+
+
+a="the future is bright"
+b=a[-9:-3]
+c=a[::-1]
+print(b)
+print(c)
+
+
+# In[41]:
+
+
+a=-5
+b=2-7
+print(a==b)
+
+
+# In[42]:
+
+
+a="hello"
+b="Hello"
+print(a==b)
+
+
+# In[ ]:
+
+
+
+
