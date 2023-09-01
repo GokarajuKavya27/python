f8692288f8dd4b616a610bdeee8570fb6fd8803e
GokarajuKavya27 <116956986+GokarajuKavya27@users.noreply.github.com>
2023-09-01 15:08:40 +0530

    Add files via upload

---
+++ basic conditional statement prgms.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a=int(input("enter a number:"))
+if a%2==0:
+    print("even")
+else:
+    print("odd")
+
+
+# In[4]:
+
+
+a=int(input("enter:"))
+b=int(input("enter:"))
+if a>b:
+    print("a is largest")
+else:
+    print("b is big")
+
+
+# In[8]:
+
+
+a=123456
+if a>0:
+    print("postive")
+elif a<0:
+    print("negative")
+else:
+    print("zero")
+
+
+# In[12]:
+
+
+a=5
+if a>0:
+    print("positive")
+    if a%2==0:
+        print("even")
+    else:
+        print("odd")
+else:
+    print("negative")
+    if a%2==0:
+        print("even")
+    else:
+        print("odd")
+
+
+# In[ ]:
+
+
+
+
