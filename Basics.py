c0c11ab58c0d0c68cd5bef69739b3e8243e5fa73
GokarajuKavya27 <116956986+GokarajuKavya27@users.noreply.github.com>
2023-09-01 13:57:57 +0530

    Add files via upload
    some of the basic programs of python for biginners

---
+++ Basics.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+a=input("enter your name:")
+print("my name is:",a)
+
+
+# In[12]:
+
+
+a=10
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+#another way
+
+sum=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print(sum)
+print(sub)
+print(mul)
+print(div)
+
+#another way
+print("Sum is:",sum)
+print("Sub is:",sub)
+print("Mul is:",mul)
+print("Divis:",div)
+
+
+# In[6]:
+
+
+name="kavya"
+print("Hii" " "+name)
+
+
+# In[7]:
+
+
+a=input("enter:")
+b=bool(a)
+print(b)
+print(type(b))
+
+
+# In[13]:
+
+
+a="my name is kavya"
+b=int(input("enter:"))
+print(a*b,end=" ")
+
+
+# In[15]:
+
+
+a=10
+b=20
+print(a+b)
+print(b-a)
+print(a*b)
+print(a%b)
+print(a//b)
+print(a/b)
+print(a**b)
+
+
+# In[21]:
+
+
+v=23
+print(v)
+v=input("enter:")
+print(v)
+
+
+# In[23]:
+
+
+r=10+3*2-8/4
+print(r)
+u=4**2+5/2*3
+print(u)
+
+
+# In[28]:
+
+
+a="kavya"
+b="gokaraju"
+print(a+" " +b)
+
+
+# In[29]:
+
+
+a=str(input("enter:"))
+b=int(input("enter:"))
+print(a*b)
+
+
+# In[32]:
+
+
+a="This is a sample sentence."
+print(len(a))
+
+
+# In[46]:
+
+
+a=23
+print(type(a))
+b=str(a)
+print(type(b))
+print(b)
+
+
+# In[45]:
+
+
+a="123"
+print(type(a))
+c=int(a)
+print(type(c))
+print(c)
+
+
+# In[ ]:
+
+
+
+
