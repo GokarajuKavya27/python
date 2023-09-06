6bef53c42bb9775013972280628970f521f36502
GokarajuKavya27 <116956986+GokarajuKavya27@users.noreply.github.com>
2023-09-06 21:53:08 +0530

    Add files via upload

---
+++ Built in functions.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+a=[1,2,6,4,5]
+print(min(a))
+print(max(a))
+print(sum(a))
+print(sorted(a))
+
+
+# In[17]:
+
+
+r=range(10)
+print(list(r))
+print(r)
+
+
+# In[2]:
+
+
+l=[]
+for i in range(1,10):
+    if i%2==0:
+        l.append(i)
+print(l)
+        
+        
+
+
+# In[19]:
+
+
+a=-10
+print(abs(a))
+
+
+# In[22]:
+
+
+a=[True,True,True]
+print(all(a))
+print(any(a))
+
+
+# In[ ]:
+
+
+
+
